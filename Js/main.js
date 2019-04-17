e9a36401ec8d702b06bf94142b12153a53e6bfd2
Siddiq Ahmed Syed <ssyed@MBP15-97HTD6-2.hsd1.ca.comcast.net>
2019-04-17 00:40:54 -0700

    updated map

---
+++ Js/main.js
@@ -0,0 +1,15 @@
+/*to show message when page is 404*/
+/*(function ($) {
+	
+var googleLink = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3172.8661662244767!2d-121.970814985427!3d37.32199774585386!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808fcaeceb6f93cf%3A0xfdae05089fc4516f!2sIslamic+Center+Of+Bozniaks!5e0!3m2!1sen!2sus!4v1555260565618!5m2!1sen!2sus";
+  $.ajax({
+     url: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3172.8661662244767!2d-121.970814985427!3d37.32199774585386!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808fcaeceb6f93cf%3A0xfdae05089fc4516f!2sIslamic+Center+Of+Bozniaks!5e0!3m2!1sen!2sus!4v1555260565618!5m2!1sen!2sus",
+     success: function (data) {
+     	console.log("Test"+data);
+        $("#gmap_iframe").html(data);
+     },
+     error: function () {
+        $("#gmap_iframe").html("Map is not avaialable");
+     }
+});
+});*/
